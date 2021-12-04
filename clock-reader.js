41581983f4654056432689d3d459d4271276bb84
Nate <nate@idontchop.com>
2021-12-04 13:36:13 -0800

    Begin work on clock model

--- clock-reader.js
+++ clock-reader.js
@@ -1,8 +1,17 @@
 
 import  { createWorker } from 'tesseract.js'
 import PSM from 'tesseract.js/src/constants/PSM.js'
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
 
 export default {   
+
+    detectObjects: async (image) => {
+        
+        let model = await cocoSsd.load()
+        let predictions = await model.detect(image)
+
+        return predictions
+    },
 
     readImage: async (image) => {
         const worker = createWorker()
@@ -12,7 +21,7 @@ export default {
         await worker.initialize('eng')
         await worker.setParameters( {
             tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
-            tessedit_char_whitelist: "$0123456789.thstrdndRemainingPlaces",
+            user_patterns_file: "./patterns.txt",
             load_system_dawg: false,
             load_freq_dawg: false
 
--- test/verifyClockReader.js
+++ test/verifyClockReader.js
@@ -1,16 +1,27 @@
 import assert from 'assert'
 import ClockReader from '../clock-reader.js'
+import fs from 'fs'
+import tf from '@tensorflow/tfjs-node'
 
 describe('Clock Reader Test', function () {
-    
+    /*
     it('should initiate', () => {
         
-        ClockReader.readImage("./images/2.jpg")
+        ClockReader.readImage("./images/4.png")
             .then( ob => console.log(ob))
 
-    }),
-    it('test chipCount', () => {
-        //let t = new TourneyChop(5000,5000,6)
+    }),*/
+    it('test objects', () => {
+
+        
+        fs.readFile('./images/3.jpg', (err,data) => {
+            if (err) throw err;
+            
+            let imgTensor = tf.node.decodeImage( new Uint8Array(data), 3);
+            ClockReader.detectObjects(imgTensor)
+                .then ( ob => console.log("object: ", ob))
+
+        })
         
     })
 })
